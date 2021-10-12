f083a3178be10c89d87cc11556253386c736a334
ahmed2nabil <ahmednabl992@gmail.com>
2021-10-12 13:50:32 +0200

    add routes for delete & update

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -6,5 +6,8 @@ var bookController = require("../controller/bookController");
 router.get("/books",bookController.getBookList);
 router.get("/books/details/:bookId",bookController.getBookDetails);
 router.post("/books/save",bookController.saveBook);
+router.put("/books/:bookId",bookController.updatesBook);
+router.delete("/books/:bookId",bookController.deleteBook);
+
 
 module.exports = router;
