d3cd7c7c61db95a8b5b5db8556bd7efd985c1fba
ahmed2nabil <ahmednabl992@gmail.com>
2021-10-17 19:24:41 +0200

    implemet swagger ui

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var cors = require("cors");
 var bodyParser = require("body-parser");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 var app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1",storeRoute);
 app.use("/api/v1",bookRoute);
